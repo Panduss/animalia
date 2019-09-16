d676613d02a07976c689af80bb1678a9af1d1452
zsuzsa <zsuzsa@qup.eu>
2019-09-16 17:43:12 +0200

    added list of all animals, separated alphabetically.

--- src/app/components/pages/animalList.ts
+++ src/app/components/pages/animalList.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import * as animalsDb from '../../domain/animal/animalsDb.json';
+import { AnimalPrototype } from '../../infrastructure/animal/prototype';
+import { IonContent } from '@ionic/angular';
 
 @Component({
     selector: 'app-animal-list',
@@ -6,7 +9,36 @@ import { Component } from '@angular/core';
 })
 
 class AnimalList {
-    public constructor() {
+
+    @ViewChild(IonContent) content!: IonContent;
+    public animals: Array<AnimalPrototype> = animalsDb.FULL_LIST;
+    public showScrollToTopButton = false;
+
+    public separateAnimalsAlphabetically(record: AnimalPrototype, recordIndex: number, records: Array<AnimalPrototype>) {
+
+        if (recordIndex === 0) {
+            return record.commonName.charAt(0).toUpperCase();
+        }
+
+        if (!records[recordIndex + 1] || !records[recordIndex + 2]) {
+            return null;
+        }
+
+        const firstPrev = records[recordIndex - 1].commonName.charAt(0);
+        const firstCurrent = record.commonName.charAt(0);
+
+        if (firstPrev !== firstCurrent) {
+            return firstCurrent.toUpperCase();
+        }
+        return null;
+    }
+
+    public logScrolling(event: CustomEvent): void {
+        this.showScrollToTopButton = event.detail.deltaY > 0;
+    }
+
+    public scrollToTop() {
+        this.content.scrollToTop(300);
     }
 }
 
