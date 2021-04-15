7c56ef056efa0d35f1cd907022cd064cdd34b983
zsuzsihudacsko <zsuzsi.hudacsko@gmail.com>
2021-04-15 09:59:10 +0200

    styling is done on the tab

--- client/src/app/components/components/tab/tab.component.ts
+++ client/src/app/components/components/tab/tab.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { Event, Router, RouterEvent } from '@angular/router';
 
 @Component({
     selector: 'custom-tab',
     templateUrl: './tab.component.html'
 })
 export class TabComponent {
+
+    selectedPath = '';
+
+    public constructor(private router: Router) {
+        this.router.events.subscribe((event: Event) => {
+            if (event instanceof RouterEvent && event.url) {
+                this.selectedPath = event.url;
+            }
+        });
+    }
+
+    public isActive(path: string): boolean {
+        return this.selectedPath.includes(path);
+    }
 }
